fba128f55a87dd7902b54d34572fe6f83547eb19
Savar1ab <savar@1ab.ru>
2025-01-24 13:15:22 +0300

    Добавлен exe-файл скрипта запуска во все каталоги фич

--- Features/ERP/RunTest.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-powershell.exe -NoProfile -ExecutionPolicy Bypass -File "D:\GitHub\vanessa-automation-script-autotest\RunScript.PS1" -PathToScript ".\Scripts\UpdateAndTestVanessa.sbsl" -PathToSettings ".\Features\ERP"
-pause
